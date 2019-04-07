ed75715cfcfe25b0ddaa477ee14bc4513971ee77
Shaun Alexander <shaun@sierraalpha.co.nz>
2019-04-07 23:23:09 +1200

    changed git_repo_create to not exit the window if insuffecient args provided

--- github_repo_create.zsh
+++ github_repo_create.zsh
@@ -9,38 +9,38 @@ function github_repo_create() {
         echo "ghrc <username> <reponame> -p -d <description>\n"
         echo "-p                = use this option to set github repo as private"
         echo "-d (optional)     = a description and requires a description after it"
-        exit
-    fi
-    user=$1
-    reponame=$2
-    private=""
-    description=""
-    (( j = 1 ))
-    for i in "$@"
-    do
-        if [[ "$i" == -p ]]; then
-            private=", \"private\":\"true\""
+    else
+        user=$1
+        reponame=$2
+        private=""
+        description=""
+        (( j = 1 ))
+        for i in "$@"
+        do
+            if [[ "$i" == -p ]]; then
+                private=", \"private\":\"true\""
+            fi
+            if [[ "$i" == -d ]]; then
+                (( j = j + 1 ))
+                description=", \"description\":\"$@[$j]\""
+                (( j = j - 1 ))
+            fi
+            (( j++ ))
+        done
+        echo "User: $user Repo Name: $reponame options: $private $description"
+        curltext="curl -u '${user}' https://api.github.com/user/repos -d '{\"name\":\"${reponame}\"${private}${description} }'"
+        echo "Curling now\n"$curltext
+        eval $curltext
+        if [[ -n "$(command git show-ref origin/$(git_current_branch) 2> /dev/null)" ]]; then
+            echo "Old origin exists, renamin original origin\ngit remote rename origin old-origin"
+            git remote rename origin old-origin
         fi
-        if [[ "$i" == -d ]]; then
-            (( j = j + 1 ))
-            description=", \"description\":\"$@[$j]\""
-            (( j = j - 1 ))
-        fi
-        (( j++ ))
-    done
-    echo "User: $user Repo Name: $reponame options: $private $description"
-    curltext="curl -u '${user}' https://api.github.com/user/repos -d '{\"name\":\"${reponame}\"${private}${description} }'"
-    echo "Curling now\n"$curltext
-    eval $curltext
-    if [[ -n "$(command git show-ref origin/$(git_current_branch) 2> /dev/null)" ]]; then
-        echo "Old origin exists, renamin original origin\ngit remote rename origin old-origin"
-        git remote rename origin old-origin
+        remotetext="git remote add origin https://github.com/${user}/${reponame}.git"
+        echo "Setting remote now\n${remotetext}"
+        eval $remotetext
+        pushlinktext="git push --set-upstream origin $(git_current_branch)"
+        echo "pushing\n${pushlinktext}"
+        eval $pushlinktext
     fi
-    remotetext="git remote add origin https://github.com/${user}/${reponame}.git"
-    echo "Setting remote now\n${remotetext}"
-    eval $remotetext
-    pushlinktext="git push --set-upstream origin $(git_current_branch)"
-    echo "pushing\n${pushlinktext}"
-    eval $pushlinktext
     unsetopt verbose
 }
