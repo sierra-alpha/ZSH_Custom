8aab0594b8f3c2aa107e359045083293d2d13b04
Shaun <shaun@SierraAlpha.co.nz>
2019-04-19 13:05:47 +1200

    testing battery colour

--- battery_prompt_colour.zsh
+++ battery_prompt_colour.zsh
@@ -3,19 +3,19 @@
 # A function to put out certain colours depending on battery percentage
 function battery_prompt_colour() {
     local percent colour
-    percent=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
-    if [[ percent -gt "90" ]]; then
+#    percent=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+#    if [[ percent -gt "90" ]]; then
         colour=$ZSH_THEME_BATTERY_COLOUR_90_to_100
-    elif [[ percent -gt "70" ]]; then
-        colour=$ZSH_THEME_BATTERY_COLOUR_70_to_90
-    elif [[ percent -gt "50" ]]; then
-        colour=$ZSH_THEME_BATTERY_COLOUR_50_to_70
-    elif [[ percent -gt "30" ]]; then
-        colour=$ZSH_THEME_BATTERY_COLOUR_30_to_50
-    elif [[ percent -gt "10" ]]; then
-        colour=$ZSH_THEME_BATTERY_COLOUR_10_to_30
-    elif [[ percent -gt "00" ]]; then
-        colour=$ZSH_THEME_BATTERY_COLOUR_00_to_10
-    fi
+#    elif [[ percent -gt "70" ]]; then
+#        colour=$ZSH_THEME_BATTERY_COLOUR_70_to_90
+#    elif [[ percent -gt "50" ]]; then
+#        colour=$ZSH_THEME_BATTERY_COLOUR_50_to_70
+#    elif [[ percent -gt "30" ]]; then
+#        colour=$ZSH_THEME_BATTERY_COLOUR_30_to_50
+#    elif [[ percent -gt "10" ]]; then
+#        colour=$ZSH_THEME_BATTERY_COLOUR_10_to_30
+#    elif [[ percent -gt "00" ]]; then
+#        colour=$ZSH_THEME_BATTERY_COLOUR_00_to_10
+#    fi
     echo "$colour"
 }
