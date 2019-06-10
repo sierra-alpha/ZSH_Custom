088468fdd87460e789cb75dea8cc680853f70f85
Shaun Alexander <shaun@sierraalpha.co.nz>
2019-06-11 09:01:00 +1200

    Created a run runcpp11 function to compile, run and delete using c++ 11 rather than make

---
+++ runcpp11.zsh
@@ -0,0 +1,14 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+
+function runcpp11() {
+	g++ -std=c++11 $1.cpp -o $1 && ./$1 && rm "$1" || rm "$1"
+}
