759db55ba65e71930f5a6046456dbd46be560965
Shaun Alexander <37967220+Sierra-Alpha@users.noreply.github.com>
2019-08-25 11:31:03 +1200

    Changed shell script to be location independent, added path to call script to comment on top.

--- ZSH_CustomInstallWinNoBatt.sh
+++ ZSH_CustomInstallWinNoBatt.sh
@@ -1,3 +1,4 @@
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/Sierra-Alpha/ZSH_Custom/edit/windowsNoBattery/ZSH_CustomInstallWinNoBatt.sh)"
 echo Please enter your user name
 read username
 echo Please enter your password
@@ -14,7 +15,7 @@ sudo apt-get install git -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo y
 echo $password
-cd .oh-my-zsh/
+cd ~/.oh-my-zsh/
 #git clone https://github.com/Sierra-Alpha/ZSH_Custom.git
 git clone --single-branch --branch windowsNoBattery https://github.com/Sierra-Alpha/ZSH_Custom.git
 mv custom custom.bak
