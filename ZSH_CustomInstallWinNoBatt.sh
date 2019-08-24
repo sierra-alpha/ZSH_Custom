aab4ad03270d0cd3a4bc145c8c45241da0c32e1d
Shaun Alexander <37967220+Sierra-Alpha@users.noreply.github.com>
2019-08-24 15:22:07 -0700

    Made an install script, currently for windowsNoBattery Branch and modified batter-Prompt_colour.zsh for a no battery system

---
+++ ZSH_CustomInstallWinNoBatt.sh
@@ -0,0 +1,25 @@
+echo Please enter your user name
+read username
+echo Please enter your password
+read password
+sudo apt-get update -y
+echo $password
+sudo apt-get install zsh -y
+chsh -s $(which zsh)
+echo $password
+exec zsh
+echo 2
+sudo apt-get install curl -y
+sudo apt-get install git -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo y
+echo $password
+cd .oh-my-zsh/
+#git clone https://github.com/Sierra-Alpha/ZSH_Custom.git
+git clone --single-branch --branch windowsNoBattery https://github.com/Sierra-Alpha/ZSH_Custom.git
+mv custom custom.bak
+mv ZSH_Custom custom
+cd ..
+mv .zshrc .zshrc.bak
+ln -s ~/.oh-my-zsh/custom/.zshrc .zshrc
+exec zsh
--- battery_prompt_colour.zsh
+++ battery_prompt_colour.zsh
@@ -2,20 +2,20 @@
 
 # A function to put out certain colours depending on battery percentage
 function battery_prompt_colour() {
-    local percent colour
-    percent=$(powershell.exe WMIC PATH Win32_Battery Get EstimatedChargeRemaining | grep -o '[0-9]*')
-    if [[ $percent -gt "90" ]]; then
+#    local percent colour
+#    percent=$(powershell.exe WMIC PATH Win32_Battery Get EstimatedChargeRemaining | grep -o '[0-9]*')
+ #   if [[ $percent -gt "90" ]]; then
         colour=$ZSH_THEME_BATTERY_COLOUR_90_to_100
-    elif [[ $percent -gt "70" ]]; then
-        colour=$ZSH_THEME_BATTERY_COLOUR_70_to_90
-    elif [[ $percent -gt "50" ]]; then
-        colour=$ZSH_THEME_BATTERY_COLOUR_50_to_70
-    elif [[ $percent -gt "30" ]]; then
-        colour=$ZSH_THEME_BATTERY_COLOUR_30_to_50
-    elif [[ $percent -gt "10" ]]; then
-        colour=$ZSH_THEME_BATTERY_COLOUR_10_to_30
-    elif [[ $percent -gt "00" ]]; then
-        colour=$ZSH_THEME_BATTERY_COLOUR_00_to_10
-    fi
+  #  elif [[ $percent -gt "70" ]]; then
+   #     colour=$ZSH_THEME_BATTERY_COLOUR_70_to_90
+#    elif [[ $percent -gt "50" ]]; then
+ #       colour=$ZSH_THEME_BATTERY_COLOUR_50_to_70
+  #  elif [[ $percent -gt "30" ]]; then
+   #     colour=$ZSH_THEME_BATTERY_COLOUR_30_to_50
+#    elif [[ $percent -gt "10" ]]; then
+ #       colour=$ZSH_THEME_BATTERY_COLOUR_10_to_30
+  #  elif [[ $percent -gt "00" ]]; then
+   #     colour=$ZSH_THEME_BATTERY_COLOUR_00_to_10
+#    fi
     echo "$colour"
 }
